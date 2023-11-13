3906b9f30e7607ba912f543fb0bc2936bcb9c66c
vicfirth@1997 <yashmundra1997@gmail.com>
2023-11-13 19:04:22 +0530

    Express started

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/gmail' ,(req,res)=>{
+    res.send('yashmundra1997@gmail.com')
+})
+
+app.get('/login' ,(req,res)=>{
+    res.send('<h1>Please login</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
